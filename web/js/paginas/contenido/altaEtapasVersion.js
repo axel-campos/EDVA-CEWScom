3544e38bc6a7e7daa3a7f24e8a95d27bd8cf9d22
DanHv <DanHv@DESKTOP-CRK7JQN>
2016-09-26 21:14:20 -0500

    Establecer tiempos de modificación por versiones

--- web/js/paginas/contenido/altaEtapasVersion.js
+++ web/js/paginas/contenido/altaEtapasVersion.js
@@ -1,104 +1,112 @@
 $(document).ready(function(){
     var form = "#frmTiempos";
     var action = "registraVersion";
-    
-    var fechaValidators = {
-        validators: {
-            notEmpty:{
-                message: 'Ingresa la fecha de terminación'
-            }
-        }
-    };
-    $(form).bootstrapValidator({
-        fields:{
-            /*etapa: {
-                validators:{
-                    greaterThan: {
-                        value: 2,
-                        message: "Por favor, ingrese el token del grupo."
+    $("#btnAgregar").on('click',function(event){
+        event.preventDefault();
+       //Aquí nos dedicamos a ingresar la nueva fila a la tabla.
+        var numeroVersion = ($("#tablaVersiones tr").length);
+        var fila = "<tr>"
+            + "<td style='text-align: center'>" + numeroVersion + "<input type='hidden' value='" + numeroVersion + "' id='version' name='version'></td>"
+            + "<td style='padding-left: 26%'><div class='form-group row-fluid'><input type='text' placeholder='Ingresa fecha' id='fecha' name='fecha' class='form-control' style='width: 60%'></div></td>"
+            + "</tr>";
+        $("#tablaVersiones tr:last").after(fila);//Agregamos la fila
+
+        $("#btnAgregar").hide();//Ocultamos esta opción.
+        $("#btnCancelar").show();
+        $("#btnSubmit").show();
+        //Agregamos la validación
+        $(form).bootstrapValidator('destroy')
+                .bootstrapValidator({
+                    fields:{
+                        fecha:{
+                            validators:{
+                                notEmpty:{
+                                    message: 'Ingrese fecha'
+                                },
+                                date:{
+                                    format: 'YYYY-MM-DD h:m',
+                                    message: 'Formato de fecha incorrecto'
+                                }
+                            }
+                        }
+                    }
+        }).off('success.form.fv').off('submit').on('submit', function(event){
+            event.preventDefault();
+            var datos = $(form).serialize();
+            //mensajes(datos, TIPO_MENSAJE.SUCCESS);
+            $.ajax({
+                type: "POST",
+                url: action,
+                data: datos,
+                success: function(data){
+                    if(data.toString() !== ""){
+                        var idContenido = $("#idContenido").val();
+                        var etapa = $("#etapa").val();
+                        if(data.toString() === "Exito"){
+                            //Refrecamos la ventana
+                            $("#ventana").load("cargaEtapas",{"idContenido": idContenido, "etapa": etapa}, function(response, status, xhr){
+                                if(status === "success"){
+                                    //Cargamos las versiones de la etapa en la que se cargo el nuevo
+                                    cargarVersiones(etapa);
+                                }
+
+                            });
+                        //Mostramos el mensaje
+                        mensajes("Éxito al registrar la versión de la etapa", TIPO_MENSAJE.SUCCESS);
+                        }else if(data.toString() === "Fecha"){
+                            var mensajito = "No se pudo establecer el tiempo límite de modificación para esta versión debido a que existe otra cuyo tiempo para modificar incluye a ésta.";
+                            mensajes(mensajito,TIPO_MENSAJE.DANGER);
+                        }
+                        
+                    }else{
+                        mensajes("Error. En este momento no podemos guardar la información de esta versión", TIPO_MENSAJE.DANGER);
                     }
                 }
-            }*/
-            temp: fechaValidators
-        }
-    }).on('success.form.fv', function(e){
-        //Prevent form submission
-        e.preventDefault();
-        //Use Ajax to submit form data
-        var datos = $(form).serialize();
-        //BootstrapDialog.alert(datos);
-        /*$.ajax({
-            type: "POST",
-            url: action,
-            data: datos,
-            success: function(data){
-                /*var target = "#contenido";
-                $(target).html(data);*
-                if(data.toString() === "Exito"){
-                    mostrarNotificacion("success",data);
-                }else{
-                    mostrarNotificacion("error","Error!");
-                }
-            }
-        });*/       
+            });
+        }).on('click','#btnCancelar',function(){
+            //Quitamos la validación
+            $(form).bootstrapValidator('removeField',$("#fecha"));
+            //Quitamos la fila que habíamos agregado a la tabla
+            $("#tablaVersiones tr:last").remove();
+            //Mostramos los botones
+            $("#btnAgregar").show();
+            //Quitamos los botones
+            $("#btnCancelar").hide();
+            $("#btnSubmit").hide();
+        });
+        
+        
+        
+        $("#fecha").datetimepicker({
+            locale: 'es',
+            format: 'YYYY-MM-DD HH:mm',
+            minDate: moment().add(1, 'days').format('YYYY-MM-DD'),
+            defaultDate: moment().add(1, 'days').format('YYYY-MM-DD')
+        }).show().on("dp.change",function(e){
+            $("#frmTiempos").bootstrapValidator('revalidateField', $("#fecha"));//Revalidamos el campo cada vez que cambie
+        }); 
     });
 });
 
-
 function cargarVersiones(etapa){
+    var form = "#frmTiempos";
     var action = "cargaVersiones.action";
     var idContenido = $("#idContenido").val();
+    $(form).bootstrapValidator('removeField',$("#fecha"));
     if(etapa === "0"){//Selecciona etapa.
         $("#btnAgregar").hide();
-        $("#btnCancelar").hide();
-        $("#btnSubmit").hide();
     }else{
         $("#btnAgregar").show();
-        $("#btnCancelar").hide();
-        $("#btnSubmit").hide();
     }
+    $("#btnCancelar").hide();
+    $("#btnSubmit").hide();
     $.post(action, {"idContenido": idContenido, "etapa": etapa}).done(function(data) 
     {
         $("#tablaVersiones").html(data);
         if(data.toString() !== ""){//No error
-            //$("#tb_grupo").html(data);
-            //$("#correcto").show();
         }else{
             
         }
     });
 }
-
-function agregarVersion(){
-    var numeroVersion = ($("#tablaVersiones tr").length);
-    var fila = "<tr>"
-        + "<td style='text-align: center'>" + numeroVersion + "<input type='hidden' value='" + numeroVersion + "' id='version' name='version'></td>"
-        + "<td style='padding-left: 20%'><div class='form-group row-fluid'><input type='text' id='fecha' name='fecha' class='form-control' style='width: 55%'></div></td>"
-        + "</tr>";
-    $("#tablaVersiones tr:last").after(fila);//Agregamos la fila
-    
-    $("#fecha").datetimepicker({
-        locale: 'es',
-        format: 'YYYY-MM-DD H:mm'
-    }).on('changeDate', function(event){
-        alert("Para vivier");
-        $("#frmTiempos").bootstrapValidator('revalidateField',$('#fecha').find('[name="fecha"]'))
-    });
-    $("#btnAgregar").hide();//Ocultamos esta opción.
-    $("#btnCancelar").show();
-    $("#btnSubmit").show();
-    var fechaValidators = {
-        validators: {
-            notEmpty:{
-                message: 'Ingresa la fecha de terminación'
-            }
-        }
-    };
-    $("#frmTiempos").bootstrapValidator('addField','fecha', fechaValidators)
-}
-
-function cancelarOperacion(){
-    alert("No mames!!!");
-}
-
 
