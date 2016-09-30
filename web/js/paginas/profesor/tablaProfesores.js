7f67d80cf8a5ca8da2b093493cfcd593db9b2983
Víctor <Vctor@Vctor-PC>
2016-09-29 19:20:15 -0500

    Algo

--- web/js/paginas/profesor/tablaProfesores.js
+++ web/js/paginas/profesor/tablaProfesores.js
@@ -1,26 +1,5 @@
 $(document).ready(function(){
-    $("#tabla").bootstrapTable({
-        height: getHeight(),
-        sortName: 'Item ID',
-        pageList: [10, 25, 50, 100, 'All'],
-        sortable: true,
-        pagination: true,
-        pageNumber: true,
-        search: true,
-        minimumCountColumns: 2,
-        formatSearch: function(){
-            return "Buscar...";
-        },
-        formatRecordsPerPage: function(pageNumber){
-            return pageNumber + " registros por página";
-        },
-        formatShowingRows: function(pageFrom, pageTo, totalRows){
-            return "Mostrando " + pageFrom + " a " + pageTo + " de " + totalRows + " registros";
-        },
-        formatNoMatches: function(){
-            return "Ningún registro coincide con la búsqueda";
-        }
-    });
+    
     var form = "#frmFiltros";
     var action = "SearchProfesor";
     
@@ -73,6 +52,29 @@ $(document).ready(function(){
                     $("#contenido").html(data);
                 }
             });
+        }
+    });
+    
+    $("#tabla").bootstrapTable({
+        height: getHeight(),
+        sortName: 'Item ID',
+        pageList: [10, 25, 50, 100, 'All'],
+        sortable: true,
+        pagination: true,
+        pageNumber: true,
+        search: true,
+        minimumCountColumns: 2,
+        formatSearch: function(){
+            return "Buscar...";
+        },
+        formatRecordsPerPage: function(pageNumber){
+            return pageNumber + " registros por página";
+        },
+        formatShowingRows: function(pageFrom, pageTo, totalRows){
+            return "Mostrando " + pageFrom + " a " + pageTo + " de " + totalRows + " registros";
+        },
+        formatNoMatches: function(){
+            return "Ningún registro coincide con la búsqueda";
         }
     });
 });
