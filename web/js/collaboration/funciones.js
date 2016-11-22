f0b4df3987336aec690c286ee747779e675a030f
kikemon <kikemon@Enrique>
2016-11-21 22:33:53 -0600

    Arreglado orden de artefactos en JSON al guardarse en Dropbox.

--- web/js/collaboration/funciones.js
+++ web/js/collaboration/funciones.js
@@ -4,53 +4,82 @@ $(document).ready(function () {
     $("#btnGuardar").click(function () {
         var listaArtefactos = MDOUtil.parseNodeList(document.querySelectorAll(".event"));
 
-        if (listaArtefactos.length > 0) {
-            var artefactos = {
-                artefactos: {
-                    ruta: RUTA_PERSISTENCIA,
-                    lista: MDOUtil.getListaArtefactos(listaArtefactos)
-                }
-            };
+		var artefactos = {
+			artefactos: {
+				ruta: RUTA_PERSISTENCIA,
+				lista: intercambiarListaPorPares(MDOUtil.getListaArtefactos(listaArtefactos))
+			}
+		};
 
-            console.log(artefactos);
-            $.post(APP_BASE + "/mdocontenido/GuardarProgreso", {
-                artefactos: JSON.stringify(artefactos),
-                titulo: TITULO,
-                version: VERSION
-            }, function (response) {
-                if (response.estatus) {
-                    BootstrapDialog.show({
-                        title: "Progreso Guardado",
-                        message: response.message,
-                        type: BootstrapDialog.TYPE_SUCCESS,
-                        buttons: [{
-							label: "Continuar Colaborando",
-							icon: "glyphicon glyphicon-ok-circle",
-							cssClass: "btn-success",
-							action: function (dialog) {
-								dialog.close();
-							}
-						}]
-                    });
-                } else {
-                    BootstrapDialog.show({
-                        title: "Error al Guardar Progreso",
-                        message: response.message,
-                        type: BootstrapDialog.TYPE_DANGER,
-                        buttons: [{
-							label: "Cerrar",
-							icon: "glyphicon glyphicon-remove-circle",
-							cssClass: "btn-danger",
-							action: function (dialog) {
-								dialog.close();
-							}
-						}]
-                    });
-                }
-            });
-        }
+		console.log(artefactos);
+		$.post(APP_BASE + "/mdocontenido/GuardarProgreso", {
+			artefactos: JSON.stringify(artefactos),
+			titulo: TITULO,
+			version: VERSION
+		}, function (response) {
+			if (response.estatus) {
+				BootstrapDialog.show({
+					title: "Progreso Guardado",
+					message: response.message,
+					type: BootstrapDialog.TYPE_SUCCESS,
+					buttons: [{
+						label: "Continuar Colaborando",
+						icon: "glyphicon glyphicon-ok-circle",
+						cssClass: "btn-success",
+						action: function (dialog) {
+							dialog.close();
+						}
+					}]
+				});
+			} else {
+				BootstrapDialog.show({
+					title: "Error al Guardar Progreso",
+					message: response.message,
+					type: BootstrapDialog.TYPE_DANGER,
+					buttons: [{
+						label: "Cerrar",
+						icon: "glyphicon glyphicon-remove-circle",
+						cssClass: "btn-danger",
+						action: function (dialog) {
+							dialog.close();
+						}
+					}]
+				});
+			}
+		});
     });
 });
+
+/**
+ * Intercambia los objetos de la lista de par en par. Por ejemplo, los
+ * elementos de la lista [a, b, c, d, ...] quedarían ordenados
+ * de la siguiente manera: [b, a, d, c, ...].
+ * 
+ * @param {array} lista La lista de elementos a intercambiar.
+ * @returns {array} La lista intercambiada.
+ */
+function intercambiarListaPorPares(lista) {
+	var length = lista.length;
+	
+	if (length === 0) {
+		return [];
+	} else if (length === 1) {
+		return lista[0];
+	} else if (length === 2) {
+		return [lista[1], lista[0]];
+	}
+	
+	var intercambiado = [];
+	
+	for (var i = 0; i < length; i += 2) {
+		if ((i + 1) < length) {
+			intercambiado.push(lista[i + 1]);
+			intercambiado.push(lista[i]);
+		}
+	}
+	
+	return (length % 2 === 0) ? intercambiado : intercambiado.concat(lista[length - 1]);
+}
 
 /**
  * Recrea la línea del tiempo a partir de los artefactos obtenidos del servidor.
@@ -59,7 +88,7 @@ $(document).ready(function () {
  * @param {string} container El contenedor donde se insertarán los artefactos.
  */
 function recrearTimeline(artefactos, container) {
-    var body = MDOTimeline.obtenerNodos(artefactos).join("");
+    var body = intercambiarListaPorPares(MDOTimeline.obtenerNodos(artefactos)).join("");
 	$(container).empty().html("<li class='year'>Inicio</li>").append(body);
 }
 
